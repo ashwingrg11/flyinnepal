e5567d61726ecf88ee8a82ab90d9ee7a55fabfb1
rejoy <rejoymistry27@gmail.com>
2018-07-31 11:14:51 +0545

    Toggle menu and hide return date for one-way integrated

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var $BUTTON_WRAPPER = $('buttons-wrapper'),
+    $BUTTON = $('button.btn'),
+    $RETURN_DATE = $('div.return-date');
+//toggle menu for active class and hide return date when one-way is active
+function init_togglemenu() {
+  $RETURN_DATE.hide();
+  $BUTTON.on('click', function(e) {
+    var $parent = $(this).parent();
+
+    $parent.find($BUTTON).removeClass('active-btn btn-secondary').addClass('way-btn');
+    $(this).removeClass('way-btn btn-light').addClass('active-btn btn-secondary');
+    var btnClicked = $(e.target).data('target');
+
+    if (btnClicked === "btn2")
+      $RETURN_DATE.show();
+    else if(btnClicked === "btn1")
+      $RETURN_DATE.hide();
+  });
+}
+
+$(document).ready(function() {
+  init_togglemenu();
+})
